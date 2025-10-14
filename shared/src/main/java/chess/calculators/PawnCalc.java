3502fd3c27e74eba3e64b42613ca989b77118cc0
ProbsNotNova <liljmackenzie@gmail.com>
2025-10-14 13:34:38 -0600

    Pawn moves passing 12 test passes

--- shared/src/main/java/chess/calculators/PawnCalc.java
+++ shared/src/main/java/chess/calculators/PawnCalc.java
@@ -6,11 +6,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class PawnCalc {
-    static void moveCheck(ChessBoard board, ChessPosition posCheck, ChessGame.TeamColor ally) {
-        if (board.getPiece(posCheck).getTeamColor() != ally || board.getPiece(posCheck).getTeamColor() == ally) {
-
-        }
-    }
+//    static void moveCheck(ChessBoard board, ChessPosition posCheck, ChessGame.TeamColor ally) {
+//        if (board.getPiece(posCheck).getTeamColor() != ally || board.getPiece(posCheck).getTeamColor() == ally) {
+//
+//        }
+//    }
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         int col = myPosition.getColumn();
         int row = myPosition.getRow();
@@ -18,7 +18,7 @@ public class PawnCalc {
         ChessPiece startPiece = board.getPiece(myPosition);
         ChessGame.TeamColor ally = startPiece.getTeamColor();
 
-        //row direction set
+        //row direction move set based on pawn color
         int rowDir = 0;
         if (ally == ChessGame.TeamColor.WHITE) {
             rowDir = 1;
@@ -26,19 +26,24 @@ public class PawnCalc {
             rowDir = -1;
         } //something broken with colors if rowDir stays 0
 
-        // First move double move if valid
 
 
-        //single moves after first/double
-        ChessPosition move = new ChessPosition(row-rowDir, col);
+        //move 1 space forward for colored piece (based on rowDir) if space empty
+        ChessPosition move = new ChessPosition(row+rowDir, col);  // changing the - to a + passed middle of board test
+                                                                            // may have backwards implementations
         if(board.getPiece(move) == null) {
             validMoves.add(new ChessMove(myPosition, move, null));
-//            if(row == 6 && board.getPiece(myPosition).getTeamColor() == ally) {
-//                if (row -
-//            } else if (row == 1 && board.getPiece(myPosition).getTeamColor() != ally){
-//                if (row + 2)
-//            }
+
+            // If pawn hasn't moved from starting row for color, validate double move space
+            ChessPosition doubleMove = new ChessPosition(row+(rowDir*2), col);
+            if((row == 7 && rowDir<0) || (row == 2 && rowDir>0)) { //code sees bottom left as {1,1}
+                if(board.getPiece(doubleMove) == null) {
+                    validMoves.add(new ChessMove(myPosition, doubleMove, null));
+                }
+            }
         }
+        // if diagonal in front of pawn contains enemy, it's a valid move
+        if ()
 
         // spot enemy (front diagonals)
 
@@ -56,28 +61,6 @@ public class PawnCalc {
 //            moveCheck(board, startMove1, ally);
 //        }
 
-//        default public boolean checkPos(ChessPosition myPosition, Collection<ChessMove> validMoves, ChessBoard board,
-//                                    ChessGame.TeamColor ally, ChessPosition posCheck) {
-//        if (board.getPiece(posCheck) == null) {
-//            validMoves.add(new ChessMove(myPosition, posCheck, null) ); // promote for Pawn
-//        } else if (board.getPiece(posCheck).getTeamColor() != ally) {
-//            validMoves.add(new ChessMove(myPosition, posCheck, null) );
-//            return false;
-//        } else if (board.getPiece(posCheck).getTeamColor() == ally) {
-//            return false;
-//        }
-//        return true;
-//    }
-
-//        int[][] directions = new int[][]{{-2,-1}, {-1, -2}, {-2, 1}, {-1, 2}, {2, -1}, {1, -2}, {2, 1}, {1, 2}};
-//        // for each part of directions in move, check for position move validity
-//        // 0 is the first [] of directions and 1 is the second [] of directions
-//        for (int[] move : directions) {
-//            if(0 < row-move[0] && row-move[0] < 9 && 0 < col-move[1] && col-move[1] < 9) {
-//                ChessPosition posCheck = new ChessPosition(row-move[0], col-move[1]);
-//                checkPos(myPosition, validMoves, board, ally, posCheck);
-//            }
-//        }
         return validMoves;
     }
 }
