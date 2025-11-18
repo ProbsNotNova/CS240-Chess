d24f03f3d108b9c3be13efaca9127a039c5dc29c
ProbsNotNova <liljmackenzie@gmail.com>
2025-11-18 10:44:49 -0700

    Finished code, time for unit tests

--- client/src/main/java/backend/ServerFacade.java
+++ client/src/main/java/backend/ServerFacade.java
@@ -10,14 +10,11 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.AbstractCollection;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Map;
 
 public class ServerFacade {
 
-    private HttpRequest buildRequest (String path, String method, Object body, String authToken) throws IOException {
+    private HttpRequest buildRequest(String path, String method, Object body, String authToken) throws IOException {
         try {
             HttpRequest.Builder builder = HttpRequest.newBuilder();
             builder.uri(new URI("http://localhost:8080/" + path));
@@ -31,17 +28,17 @@ public class ServerFacade {
         }
     }
 
-    private HttpRequest.BodyPublisher bodyBuilder (Object body) {
+    private HttpRequest.BodyPublisher bodyBuilder(Object body) {
         if (body == null) {
             return HttpRequest.BodyPublishers.noBody();
         }
         return HttpRequest.BodyPublishers.ofString(new Gson().toJson(body));
     }
 
-    private <T> T sendRequest (HttpRequest request, Class<T> responseClass) throws IOException {
+    private <T> T sendRequest(HttpRequest request, Class<T> responseClass) throws IOException {
         try (HttpClient client = HttpClient.newHttpClient()) {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            if(response.statusCode() == 200) {
+            if (response.statusCode() == 200) {
                 if (responseClass == null) {
                     return null;
                 }
@@ -57,13 +54,14 @@ public class ServerFacade {
     public String register(String uName, String pWord, String email) throws IOException {
         HttpRequest request = buildRequest("user", "POST", new UserData(uName, pWord, email), null);
         AuthData auth = sendRequest(request, AuthData.class);
-        assert auth != null; ////////
+        assert auth != null;
         return auth.authToken();
     }
+
     public String login(String uName, String pWord) throws IOException {
         HttpRequest request = buildRequest("session", "POST", new UserData(uName, pWord, null), null);
         AuthData auth = sendRequest(request, AuthData.class);
-        assert auth != null; ////////
+        assert auth != null;
         return auth.authToken();
     }
 
@@ -75,28 +73,17 @@ public class ServerFacade {
     public int createGame(String gameName, String authToken) throws IOException {
         HttpRequest request = buildRequest("game", "POST", new CreateGameRequest(gameName), authToken);
         GameData game = sendRequest(request, GameData.class);
-        assert game != null; //////////
+        assert game != null;
         return game.gameID();
     }
     public Collection<GameData> listGames(String authToken) throws IOException {
         HttpRequest request = buildRequest("game", "GET", null, authToken);
         ListGamesResult games = sendRequest(request, ListGamesResult.class);
-        assert games != null; ////////
+        assert games != null;
         return games.games();
     }
     public void logout(String authToken) throws IOException {
         HttpRequest request = buildRequest("session", "DELETE", null, authToken);
         sendRequest(request, AuthData.class);
-//        assert auth != null; ////////
-//        return auth.authToken();
     }
-    
-//     // Server Endpoints
-//        //javalin.post("/user", this::registerHandler);
-//        //javalin.post("/session", this::loginHandler);
-//        //javalin.delete("/session", this::logoutHandler);
-//        //javalin.get("/game", this::listGamesHandler);
-//        javalin.post("/game", this::createGameHandler);
-//        javalin.put("/game", this::joinGameHandler);
-///        javalin.delete("/db", this::clearDataBaseHandler);
 }
--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -52,7 +52,6 @@ public class ChessClient {
             case "join" -> joinGame(params);
             case "observe" -> observeGame(params);
             case "logout" -> logout();
-            case "quit" -> "quit";
             case "help" -> help();
             default -> help();
         };
@@ -64,7 +63,7 @@ public class ChessClient {
                     - login <username> <password>
                     - register <username> <password> <email>
                     - help - with possible commands
-                    - quit
+                    - quit - the application
                     """;
         }
         return """
@@ -73,7 +72,6 @@ public class ChessClient {
                 - join <ID> [WHITE|BLACK]
                 - observe <ID> - a game
                 - logout - when you are done
-                - quit - playing chess
                 - help - with possible commands
                 """;
     }
@@ -117,9 +115,9 @@ public class ChessClient {
         }
         Collection<GameData> games = server.listGames(sessionAuth);
         var result = new StringBuilder();
-        var gson = new Gson();
         for (GameData game : games) {
-            result.append(gson.toJson(game)).append('\n');
+            result.append("GameID: ").append(game.gameID()).append(" | Gamename: ").append(game.gameName());
+            result.append(" | WHITE: ").append(game.whiteUsername()).append(" | BLACK: ").append(game.blackUsername()).append('\n');
         }
         return result.toString();
     }
@@ -134,9 +132,14 @@ public class ChessClient {
         return String.format("Joined game %s as %s team", game.gameName(), params[1]);
     }
     public String observeGame(String... params) throws IOException {
-//        GameData game = getGame(params[0]); function that gets a game to observe without joining
-        bdPrint.printBoard(params[0], null);
-        return String.format("Observing game %s", "GAME" /*game.gameName()*/, params[0]);
+        assertSignedIn();
+        if (params.length != 1) {
+            throw new IOException("Invalid Parameters, Check help()");
+        }
+        Collection<GameData> games = server.listGames(sessionAuth);
+        GameData game = (GameData) games.toArray()[parseInt(params[0])-1];
+        bdPrint.printBoard("WHITE", game.game().getBoard());
+        return String.format("Observing game with ID %s", params[0]);
     }
     public String logout(String... params) throws IOException {
         assertSignedIn();
@@ -149,141 +152,9 @@ public class ChessClient {
         return "Logged out";
     }
 
-
-
-//
-//    public String adoptPet(String... params) throws ResponseException {
-//        assertSignedIn();
-//        if (params.length == 1) {
-//            try {
-//                int id = Integer.parseInt(params[0]);
-//                Pet pet = getPet(id);
-//                if (pet != null) {
-//                    server.deletePet(id);
-//                    return String.format("%s says %s", pet.name(), pet.sound());
-//                }
-//            } catch (NumberFormatException ignored) {
-//            }
-//        }
-//        throw new ResponseException(ResponseException.Code.ClientError, "Expected: <pet id>");
-//    }
-
-
-//
-//    private Pet getPet(int id) throws ResponseException {
-//        for (Pet pet : server.listPets()) {
-//            if (pet.id() == id) {
-//                return pet;
-//            }
-//        }
-//        return null;
-//    }
-//
-
     private void assertSignedIn() throws IOException {
         if (state == State.SIGNEDOUT) {
             throw new IOException("You must login or register");
         }
     }
 }
-
-//package client;
-//
-//import java.util.Arrays;
-//import java.util.Scanner;
-//
-//import com.google.gson.Gson;
-//import model.*;
-//import exception.ResponseException;
-//import client.websocket.NotificationHandler;
-//import server.ServerFacade;
-//import client.websocket.WebSocketFacade;
-//import webSocketMessages.Notification;
-//
-//import static client.EscapeSequences.*;
-
-//
-//    public String signIn(String... params) throws ResponseException {
-//        if (params.length >= 1) {
-//            state = State.SIGNEDIN;
-//            visitorName = String.join("-", params);
-//            ws.enterPetShop(visitorName);
-//            return String.format("You signed in as %s.", visitorName);
-//        }
-//        throw new ResponseException(ResponseException.Code.ClientError, "Expected: <yourname>");
-//    }
-//
-//    public String rescuePet(String... params) throws ResponseException {
-//        assertSignedIn();
-//        if (params.length >= 2) {
-//            String name = params[0];
-//            PetType type = PetType.valueOf(params[1].toUpperCase());
-//            var pet = new Pet(0, name, type);
-//            pet = server.addPet(pet);
-//            return String.format("You rescued %s. Assigned ID: %d", pet.name(), pet.id());
-//        }
-//        throw new ResponseException(ResponseException.Code.ClientError, "Expected: <name> <CAT|DOG|FROG>");
-//    }
-//
-//    public String listPets() throws ResponseException {
-//        assertSignedIn();
-//        PetList pets = server.listPets();
-//        var result = new StringBuilder();
-//        var gson = new Gson();
-//        for (Pet pet : pets) {
-//            result.append(gson.toJson(pet)).append('\n');
-//        }
-//        return result.toString();
-//    }
-//
-//    public String adoptPet(String... params) throws ResponseException {
-//        assertSignedIn();
-//        if (params.length == 1) {
-//            try {
-//                int id = Integer.parseInt(params[0]);
-//                Pet pet = getPet(id);
-//                if (pet != null) {
-//                    server.deletePet(id);
-//                    return String.format("%s says %s", pet.name(), pet.sound());
-//                }
-//            } catch (NumberFormatException ignored) {
-//            }
-//        }
-//        throw new ResponseException(ResponseException.Code.ClientError, "Expected: <pet id>");
-//    }
-//
-//    public String adoptAllPets() throws ResponseException {
-//        assertSignedIn();
-//        var buffer = new StringBuilder();
-//        for (Pet pet : server.listPets()) {
-//            buffer.append(String.format("%s says %s%n", pet.name(), pet.sound()));
-//        }
-//
-//        server.deleteAllPets();
-//        return buffer.toString();
-//    }
-//
-//    public String signOut() throws ResponseException {
-//        assertSignedIn();
-//        ws.leavePetShop(visitorName);
-//        state = State.SIGNEDOUT;
-//        return String.format("%s left the shop", visitorName);
-//    }
-//
-//    private Pet getPet(int id) throws ResponseException {
-//        for (Pet pet : server.listPets()) {
-//            if (pet.id() == id) {
-//                return pet;
-//            }
-//        }
-//        return null;
-//    }
-//
-
-//
-//    private void assertSignedIn() throws ResponseException {
-//        if (state == State.SIGNEDOUT) {
-//            throw new ResponseException(ResponseException.Code.ClientError, "You must sign in");
-//        }
-//    }
-//}
